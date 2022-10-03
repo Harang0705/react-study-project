15b62f5151c13cb5033af016d9b07303ad6fb3d9
Harang0705 <t62711691@gmail.com>
2022-10-03 22:13:01 +0900

    describe (기본설명) 페이지 완성

--- src/App.js
+++ src/App.js
@@ -1,30 +1,57 @@
 import React, { Component } from 'react';
-import style from './App.module.css';
+import header from './header.module.css';
+import describe from './describe.module.css';
 import logo from './logo.svg'; // 107 px * 70 px image svg
+import describeLogo from './describeLogo.png';
+import downPageBtn from './downPageBtn.png';
 
 class App extends Component {
   render() {
     return (
-      <div className={style.mainInner}>
-        <div className={style.header}> // header
-          <ul className={style.mainbar}>
-            <li className={style.logo}>
+      <div className={header.mainInner}>
+        <div className={header.inner}>
+          <ul className={header.mainNav}>
+            <li className={header.logo}>
               <img src={logo}></img>
             </li>
-            <li className={style.description}>
-              <a className={style.text}>설명</a>
+            <li className={header.description}>
+              <a className={header.text}>설명</a>
             </li>
-            <li className={style.vision}>
-              <a className={style.text}>목표</a>
+            <li className={header.vision}>
+              <a className={header.text}>목표</a>
             </li>
-            <li className={style.appDownload}>
-              <a className={style.text}>앱 다운로드</a>
+            <li className={header.appDownload}>
+              <a className={header.text}>앱 다운로드</a>
             </li>
           </ul>
+        </div>
+        <div className={describe.inner}>
+          <div className={describe.content}>
+            <img className={describe.logo} src={describeLogo}></img>
+            <div className={describe.text}>
+              <div className={describe.textBox}>
+                <a>서로가 행복한 My Pet 프로젝트</a>
+                <br/> 
+                <a>우리의 목표를 따라와볼래요?</a>
+              </div>
+              <div className={describe.textBox}>
+                <a>서로가 행복한 My Pet 프로젝트</a>
+                <br/> 
+                <a>우리의 목표를 따라와볼래요?</a>
+              </div>
+            </div>
+          </div>
+          <div className={describe.pageDownButton}>
+            <img id='downPage' src={downPageBtn}></img>
+          </div>
         </div>
       </div>
     );
   }
 }
+
+document.getElementById('downPage', function () {
+  
+});
 
 export default App
